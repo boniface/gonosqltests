4e3c36eee5bd5a91b7327b84e9c6a3f1448399d0
boniface <boniface@kabaso.com>
2018-07-19 17:20:00 +0200

    Added DB Drivers

--- api/index.go
+++ api/index.go
@@ -4,10 +4,8 @@ import "github.com/kataras/iris"
 
 //Index: landing page
 func Index(ctx iris.Context) {
-		// Bind: {{.message}} with "Hello world!"
-		ctx.ViewData("message", "Hello world!")
-		// Render template file: ./views/hello.html
-		ctx.View("index.html")
-	}
-
-
+	// Bind: {{.message}} with "Hello world!"
+	ctx.ViewData("message", "Hello world! Test Testing ")
+	// Render template file: ./views/hello.html
+	ctx.View("index.html")
+}
