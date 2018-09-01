3384a7e831f3e1b1cbd53957196a20bc3fc0cff5
boniface <boniface@kabaso.com>
2018-09-01 13:48:10 +0200

    test for Golang

--- repository/cassandra.go
+++ repository/cassandra.go
@@ -10,7 +10,7 @@ var Session *gocql.Session
 
 func getCluster() *gocql.Session {
 	var err error
-	cluster := gocql.NewCluster("192.168.1.1", "192.168.1.2", "192.168.1.3")
+	cluster := gocql.NewCluster("localhost")
 	cluster.Keyspace = "nosqltest"
 	Session, err = cluster.CreateSession()
 	if err != nil {
@@ -31,20 +31,51 @@ func create(person domain.Person) domain.Person {
 	return person
 }
 
-func upodate(person domain.Person) domain.Person {
-	fmt.Println(" **** Creating new person ****\n", person)
+func update(person domain.Person) domain.Person {
+	fmt.Println(" **** Updating   person ****\n", person)
+	updatedPerson := domain.Person{ID: person.ID, Name: person.Name + " Updated"}
+	defer getCluster().Close()
+	if err := Session.Query("UPDATE persons SET name = ? WHERE empid = ?",
+		updatedPerson.ID, updatedPerson.Name).Exec(); err != nil {
+		fmt.Println("Error while updating Emp")
+		fmt.Println(err)
+	}
+	return updatedPerson
 
 }
 
 func delete(person domain.Person) bool {
+	fmt.Println(" **** Deleting Person  ****\n", person)
+	defer getCluster().Close()
+	if err := Session.Query("DELETE FROM persons WHERE id = ?", person.ID).Exec(); err != nil {
+		fmt.Println("Error while deleting Emp")
+		fmt.Println(err)
+	}
+	return true
 
 }
 
 func read(id string) domain.Person {
 
+	fmt.Println(" **** Reading a person with ID ****\n", id)
+	defer getCluster().Close()
+	var person domain.Person
+	m := map[string]interface{}{}
+	iter := getCluster().Query("SELECT * FROM persons WHERE id=? LIMIT 1").Iter()
+	for iter.MapScan(m) {
+		person = domain.Person{
+			ID:   m["id"].(string),
+			Name: m["name"].(string),
+		}
+		m = map[string]interface{}{}
+	}
+	return person
+
 }
 
 func readAll() []domain.Person {
+	fmt.Println(" **** Reading All Records ****\n")
+	defer getCluster().Close()
 	var persons []domain.Person
 	m := map[string]interface{}{}
 	defer getCluster().Close()
