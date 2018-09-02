f14042048c07e3ce6aacf123080bb9f13bb875f7
boniface <boniface@kabaso.com>
2018-09-02 10:20:01 +0200

    test for Golang

---
+++ services/cassandra.go
@@ -0,0 +1,10 @@
+package services
+
+import "github.com/boniface/gonosqltests/domain"
+
+func (person domain.Person) create() domain.Results {
+	results := domain.Results{}
+
+	return results
+
+}
--- services/services.go
+++ services/services.go
@@ -1,1 +1,11 @@
 package services
+
+import "github.com/boniface/gonosqltests/domain"
+
+type Services interface {
+	create() domain.Results
+	read() domain.Results
+	readAll() domain.Results
+	delete() domain.Results
+	update() domain.Results
+}
